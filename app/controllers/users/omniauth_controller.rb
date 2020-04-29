41acb2004fdc3896d5c07dc9ddca3da91990caf1
chitresh97 <chitu1997@gmail.com>
2020-04-29 19:54:58 +0530

    user sign in and sign out to custom path

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -2,8 +2,8 @@ class Users::OmniauthController < ApplicationController
     def facebook
         @user = User.create_from_provider_data(request.env['omniauth.auth'])
         if @user.persisted?
-          sign_in @user
-          redirect_to home_index_path
+          sign_in_and_redirect @user
+          # redirect_to home_index_path
         #   set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
         else
           flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
